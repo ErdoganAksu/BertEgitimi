694696ab4073b9c4292c257bb62742926d1f4f1c
ErdoganAksu <73472038+ErdoganAksu@users.noreply.github.com>
2022-05-17 10:52:57 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='BertEgitimi',
+    version='0.0.1',
+    author='Erdogan',
+    author_email='erdogan-aksu@hotmail.com',
+    url='www.example.com',
+    license='LICENSE',
+    packages=[''],
+    description='Bert Eğitimi Python package.',
+    install_requires=[
+    ]
+)
